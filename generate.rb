33e34bcd2aac7b7b8668dc7833ecd70ac78a4239
Mike McQuaid <mike@mikemcquaid.com>
2018-05-08 19:36:55 -0400

    Initial commit

---
+++ generate.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env brew ruby
+FileUtils.mkdir_p(["_data/formulae", "api/formulae", "formulae"])
+
+Formula.each do |f|
+  json_filename = "#{f.name}.json"
+  IO.write("_data/formulae/#{json_filename}", JSON.pretty_generate(f.to_hash))
+  FileUtils.cp "_api_formulae.json.in", "api/formulae/#{json_filename}"
+
+  html = IO.read "_formulae.html.in"
+  html.gsub!("title: $TITLE", "title: #{f.name}")
+  IO.write("formulae/#{f.name}.html", html)
+end
