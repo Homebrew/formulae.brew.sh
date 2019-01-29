526931a56332e3da221b2abb8b6bb1399b2e9f82
Christian Bernard <christian@automox.com>
2019-01-29 13:07:52 -0700

    Move require 'cask/cask' to appropriate section. Remove commented/todo lines.

--- script/generate.rb
+++ script/generate.rb
@@ -1,5 +1,4 @@
 #!/usr/bin/env brew ruby
-require 'cask/cask'
 directories = ["_data/formula", "api/formula", "formula", "_data/cask", "api/cask", "cask"]
 FileUtils.rm_rf directories
 FileUtils.mkdir_p directories
@@ -14,13 +13,9 @@ Formula.each do |f|
   IO.write("formula/#{f.name}.html", html)
 end
 
+require 'cask/cask'
 Cask::Cask.each do |c|
   json_filename = "#{File.basename(c.sourcefile_path,'.rb')}.json"
   IO.write("_data/cask/#{json_filename}", JSON.pretty_generate(c.to_h))
   FileUtils.cp "_api_formula.json.in", "api/cask/#{json_filename}"
-
-  # TODO: generate cask html template and output(s)
-  # html = IO.read "_formula.html.in"
-  # html.gsub!("title: $TITLE", "title: #{c.name}")
-  # IO.write("cask/#{c.name}.html", html)
 end
