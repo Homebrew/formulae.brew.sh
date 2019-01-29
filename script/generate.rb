72c2870ca70809b2ad3c4d40a9c2252cd40fe901
Christian Bernard <christian@automox.com>
2019-01-29 10:58:52 -0700

    Update generate.rb to create json files for casks in new directories for API.

--- script/generate.rb
+++ script/generate.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env brew ruby
-directories = ["_data/formula", "api/formula", "formula"]
+require 'cask/cask'
+directories = ["_data/formula", "api/formula", "formula", "_data/cask", "api/cask", "cask"]
 FileUtils.rm_rf directories
 FileUtils.mkdir_p directories
 
@@ -11,4 +12,15 @@ Formula.each do |f|
   html = IO.read "_formula.html.in"
   html.gsub!("title: $TITLE", "title: #{f.name}")
   IO.write("formula/#{f.name}.html", html)
+end
+
+Cask::Cask.each do |c|
+  json_filename = "#{File.basename(c.sourcefile_path,'.rb')}.json"
+  IO.write("_data/cask/#{json_filename}", JSON.pretty_generate(c.to_h))
+  FileUtils.cp "_api_formula.json.in", "api/cask/#{json_filename}"
+
+  # TODO: generate cask html template and output(s)
+  # html = IO.read "_formula.html.in"
+  # html.gsub!("title: $TITLE", "title: #{c.name}")
+  # IO.write("cask/#{c.name}.html", html)
 end
