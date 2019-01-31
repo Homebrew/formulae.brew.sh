d341dddc3947a24e3808c3f9caa484509fdc733c
Sean Molenaar <sean@seanmolenaar.eu>
2019-01-31 21:57:39 +0100

    Agregate formula data into algolia for web search

--- script/generate-cask.rb
+++ script/generate-cask.rb
@@ -4,8 +4,20 @@ directories = ["_data/cask", "api/cask", "cask"]
 FileUtils.rm_rf directories
 FileUtils.mkdir_p directories
 
+cask_indexes = []
 Cask::Cask.each do |c|
   json_filename = "#{c.token}.json"
   IO.write("_data/cask/#{json_filename}", JSON.pretty_generate(c.to_h))
   FileUtils.cp "_api_cask.json.in", "api/cask/#{json_filename}"
+  cask_indexes << c.to_h unless ENV["HOMEBREW_ALGOLIA_API_KEY"]
+end
+
+if ENV["HOMEBREW_ALGOLIA_API_KEY"]
+  Homebrew.install_gem! 'algoliasearch'
+  require 'algoliasearch'
+  Algolia.init(application_id: 'LV7WOXOQ3N',
+               api_key:        ENV["HOMEBREW_ALGOLIA_API_KEY"])
+  index = Algolia::Index.new('casks')
+  index.clear_index()
+  index.add_objects(cask_indexes)
 end
--- script/generate.rb
+++ script/generate.rb
@@ -3,6 +3,7 @@ directories = ["_data/formula", "api/formula", "formula"]
 FileUtils.rm_rf directories
 FileUtils.mkdir_p directories
 
+formulae_indexes = []
 Formula.each do |f|
   json_filename = "#{f.name}.json"
   IO.write("_data/formula/#{json_filename}", JSON.pretty_generate(f.to_hash))
@@ -11,4 +12,15 @@ Formula.each do |f|
   html = IO.read "_formula.html.in"
   html.gsub!("title: $TITLE", "title: #{f.name}")
   IO.write("formula/#{f.name}.html", html)
+  formulae_indexes << f.to_hash unless ENV["HOMEBREW_ALGOLIA_API_KEY"]
+end
+
+if ENV["HOMEBREW_ALGOLIA_API_KEY"]
+  Homebrew.install_gem! 'algoliasearch'
+  require 'algoliasearch'
+  Algolia.init(application_id: 'LV7WOXOQ3N',
+               api_key:        ENV["HOMEBREW_ALGOLIA_API_KEY"])
+  index = Algolia::Index.new('formulae')
+  index.clear_index()
+  index.add_objects(formulae_indexes)
 end
