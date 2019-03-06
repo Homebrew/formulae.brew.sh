dd9eb82697773aefb373df3763484ba7d4d460f6
Christian Bernard <christian@automox.com>
2019-03-06 10:31:26 -0700

    updated with suggestion.

--- script/generate-cask.rb
+++ script/generate-cask.rb
@@ -6,7 +6,6 @@ FileUtils.mkdir_p directories
 
 Cask::Cask.each do |c|
   json_filename = "#{c.token}.json"
-  IO.write("_data/cask/#{json_filename}", JSON.pretty_generate(c.to_h))
-  File.open("_data/cask/#{json_filename}",'a') { |f| f.write("\n") }
+  IO.write("_data/cask/#{json_filename}", {JSON.pretty_generate(c.to_h)}\n)
   FileUtils.cp "_api_cask.json.in", "api/cask/#{json_filename}"
 end
