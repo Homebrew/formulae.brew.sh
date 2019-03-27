b6e443a48bb15564e8cfd6ee5d1ba0c47fcc6a60
EricFromCanada <enk3@outlook.com>
2019-03-27 16:34:56 -0400

    replace plus signs with underscores in data filenames

--- script/generate.rb
+++ script/generate.rb
@@ -6,7 +6,7 @@ FileUtils.mkdir_p directories
 CoreTap.instance.formula_names.each do |n|
   f = Formulary.factory(n)
   json_filename = "#{f.name}.json"
-  IO.write("_data/formula/#{json_filename}", JSON.pretty_generate(f.to_hash))
+  IO.write("_data/formula/#{json_filename.gsub('+', '_')}", JSON.pretty_generate(f.to_hash))
   FileUtils.cp "_api_formula.json.in", "api/formula/#{json_filename}"
 
   html = IO.read "_formula.html.in"
