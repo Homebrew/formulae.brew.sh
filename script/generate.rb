b842cced13c939ef683054fcd752b5bcfe4cb926
EricFromCanada <enk3@outlook.com>
2019-07-26 01:48:35 -0400

    optimize generation scripts

--- script/generate.rb
+++ script/generate.rb
@@ -3,13 +3,12 @@ directories = ["_data/formula", "api/formula", "formula"]
 FileUtils.rm_rf directories
 FileUtils.mkdir_p directories
 
+json_template = IO.read "_api_formula.json.in"
+html_template = IO.read "_formula.html.in"
+
 CoreTap.instance.formula_names.each do |n|
   f = Formulary.factory(n)
-  json_filename = "#{f.name}.json"
-  IO.write("_data/formula/#{json_filename.gsub('+', '_')}", JSON.pretty_generate(f.to_hash))
-  FileUtils.cp "_api_formula.json.in", "api/formula/#{json_filename}"
-
-  html = IO.read "_formula.html.in"
-  html.gsub!("title: $TITLE", "title: \"#{f.name}\"")
-  IO.write("formula/#{f.name}.html", html)
+  IO.write("_data/formula/#{f.name.tr("+", "_")}.json", JSON.pretty_generate(f.to_hash))
+  IO.write("api/formula/#{f.name}.json", json_template)
+  IO.write("formula/#{f.name}.html", html_template.gsub("title: $TITLE", "title: \"#{f.name}\""))
 end
