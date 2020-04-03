55fd05074f8f7e7c124cedb060c86dabd49ece9e
Jason Karns <jason.karns@gmail.com>
2020-04-03 11:00:20 -0400

    Makes cask task accept tap argument

--- script/generate-cask.rb
+++ script/generate-cask.rb
@@ -1,5 +1,9 @@
 #!/usr/bin/env brew ruby
 require "cask/cask"
+
+tap_name = (ARGV.first || "").split("/")
+tap = tap_name.empty? ? Tap.default_cask_tap : Tap.new(*tap_name)
+
 directories = ["_data/cask", "api/cask", "cask"]
 FileUtils.rm_rf directories
 FileUtils.mkdir_p directories
@@ -7,7 +11,7 @@ FileUtils.mkdir_p directories
 json_template = IO.read "_api_cask.json.in"
 html_template = IO.read "_cask.html.in"
 
-Tap.default_cask_tap.cask_files.each do |p|
+tap.cask_files.each do |p|
   c = Cask::CaskLoader::FromPathLoader.new(p).load
   IO.write("_data/cask/#{c.token}.json", "#{JSON.pretty_generate(c.to_h)}\n")
   IO.write("api/cask/#{c.token}.json", json_template)
