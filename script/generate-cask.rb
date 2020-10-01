dba277f2c661dbdcfa8c52bf91a574ca8a489155
Markus Reiter <me@reitermark.us>
2020-10-01 21:05:56 +0200

    Fix code style.

--- script/generate-cask.rb
+++ script/generate-cask.rb
@@ -12,7 +12,7 @@ json_template = IO.read "_api_cask.json.in"
 html_template = IO.read "_cask.html.in"
 
 tap.cask_files.each do |p|
-  c = Cask::CaskLoader::load(p)
+  c = Cask::CaskLoader.load(p)
   IO.write("_data/cask/#{c.token}.json", "#{JSON.pretty_generate(c.to_h)}\n")
   IO.write("api/cask/#{c.token}.json", json_template)
   IO.write("cask/#{c.token}.html", html_template.gsub("title: $TITLE", "title: \"#{c.token}\""))
