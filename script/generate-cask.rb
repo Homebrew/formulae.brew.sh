26e30af656fd0d19bd7eb2dac7a3654e2886b8ac
Jonathan Chang <me@jonathanchang.org>
2020-10-01 23:06:34 +1000

    Fix missing config argument for cask load
    Fixes CI error:
    
    brew ruby script/generate-cask.rb Homebrew/cask
    /usr/local/Homebrew/Library/Homebrew/cask/cask_loader.rb:62:in `load': missing keyword: config (ArgumentError)

--- script/generate-cask.rb
+++ script/generate-cask.rb
@@ -12,7 +12,7 @@ json_template = IO.read "_api_cask.json.in"
 html_template = IO.read "_cask.html.in"
 
 tap.cask_files.each do |p|
-  c = Cask::CaskLoader::FromPathLoader.new(p).load
+  c = Cask::CaskLoader::FromPathLoader.new(p).load(config: Cask::Config.from_json("{}"))
   IO.write("_data/cask/#{c.token}.json", "#{JSON.pretty_generate(c.to_h)}\n")
   IO.write("api/cask/#{c.token}.json", json_template)
   IO.write("cask/#{c.token}.html", html_template.gsub("title: $TITLE", "title: \"#{c.token}\""))
