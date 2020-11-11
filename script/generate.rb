5f23a3edd5775b99757aef992c6a7c3313d674cc
EricFromCanada <enk3@outlook.com>
2020-11-11 08:55:13 -0500

    find canonical formula name if specified with alias

--- script/generate.rb
+++ script/generate.rb
@@ -6,7 +6,7 @@ formula_dir = os == "mac" ? "formula" : "formula-linux"
 tap = Tap.fetch(tap_name)
 
 directories = ["_data/#{formula_dir}", "api/#{formula_dir}", "#{formula_dir}"]
-FileUtils.rm_rf directories
+FileUtils.rm_rf directories + ["_data/#{formula_dir}_canonical.json"]
 FileUtils.mkdir_p directories
 
 json_template = IO.read "_api_formula.json.in"
@@ -18,3 +18,4 @@ tap.formula_names.each do |n|
   IO.write("api/#{formula_dir}/#{f.name}.json", json_template)
   IO.write("#{formula_dir}/#{f.name}.html", html_template.gsub("title: $TITLE", "title: \"#{f.name}\""))
 end
+IO.write("_data/#{formula_dir}_canonical.json", "#{JSON.pretty_generate(tap.formula_renames.merge(tap.alias_table))}\n")
