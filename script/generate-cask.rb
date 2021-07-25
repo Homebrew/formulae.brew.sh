137f8d43b20f1c575c54eccaa0811acc45c8a962
Rylan Polster <rslpolster@gmail.com>
2021-07-25 12:25:53 -0400

    Add cask sources API

--- script/generate-cask.rb
+++ script/generate-cask.rb
@@ -4,7 +4,7 @@ require "cask/cask"
 tap_name = ARGV.first
 tap = Tap.fetch(tap_name)
 
-directories = ["_data/cask", "api/cask", "cask"]
+directories = ["_data/cask", "api/cask", "api/cask-source", "cask"]
 FileUtils.rm_rf directories
 FileUtils.mkdir_p directories
 
@@ -15,5 +15,6 @@ tap.cask_files.each do |p|
   c = Cask::CaskLoader.load(p)
   IO.write("_data/cask/#{c.token}.json", "#{JSON.pretty_generate(c.to_h)}\n")
   IO.write("api/cask/#{c.token}.json", json_template)
+  IO.write("api/cask-source/#{c.token}.rb", p.read)
   IO.write("cask/#{c.token}.html", html_template.gsub("title: $TITLE", "title: \"#{c.token}\""))
 end
