1abe0bc9bc8d9a49a6a3947b1a3c4cfabb7bfcd1
Rylan Polster <rslpolster@gmail.com>
2022-07-22 18:34:03 +0200

    Use `#to_hash_with_variations` to generate formula and cask JSON

--- script/generate-cask.rb
+++ script/generate-cask.rb
@@ -13,7 +13,7 @@ html_template = IO.read "_cask.html.in"
 
 tap.cask_files.each do |p|
   c = Cask::CaskLoader.load(p)
-  IO.write("_data/cask/#{c.token}.json", "#{JSON.pretty_generate(c.to_h)}\n")
+  IO.write("_data/cask/#{c.token}.json", "#{JSON.pretty_generate(c.to_hash_with_variations)}\n")
   IO.write("api/cask/#{c.token}.json", json_template)
   IO.write("api/cask-source/#{c.token}.rb", p.read)
   IO.write("cask/#{c.token}.html", html_template.gsub("title: $TITLE", "title: \"#{c.token}\""))
