8675eeec3927de0fdb152be65a192c1f1ffb9aa9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-05 21:18:51 +0000

    cask: updates from homebrew/cask

---
+++ api/cask-source/readdle-spark.rb
@@ -0,0 +1,28 @@
+cask "readdle-spark" do
+  version "3.0.30.37498"
+  sha256 "96bef0b0b73660979debd84da242a51f82e7497f002beff2210bec60659abb5b"
+
+  url "https://downloads.sparkmailapp.com/Spark#{version.major}/mac/dist/#{version}/Spark.zip"
+  name "Spark"
+  desc "Email client"
+  homepage "https://sparkmailapp.com/"
+
+  livecheck do
+    url "https://downloads.sparkmailapp.com/Spark#{version.major}/mac/dist/appcast.xml"
+    strategy :sparkle
+  end
+
+  auto_updates true
+  depends_on macos: ">= :el_capitan"
+
+  app "Spark Desktop.app"
+
+  zap trash: [
+    "~/Library/Application Support/Spark Desktop",
+    "~/Library/Caches/com.readdle.SparkDesktop.helper",
+    "~/Library/Caches/Spark Desktop",
+    "~/Library/Logs/Spark Desktop",
+    "~/Library/Preferences/com.readdle.SparkDesktop.plist",
+    "~/Library/Saved Application State/com.readdle.SparkDesktop.savedState",
+  ]
+end
