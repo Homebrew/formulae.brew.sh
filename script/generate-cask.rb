b74a96c37107bcdc10847ae04d679e60c841af1f
Mike McQuaid <mike@mikemcquaid.com>
2023-02-14 16:58:55 +0000

    generate-cask: enable `Cask.generating_hash!` mode
    See https://github.com/Homebrew/brew/pull/14615
    
    This will set placeholder values in the generated hash.

--- script/generate-cask.rb
+++ script/generate-cask.rb
@@ -11,6 +11,8 @@ FileUtils.mkdir_p directories
 json_template = IO.read "_api_cask.json.in"
 html_template = IO.read "_cask.html.in"
 
+Cask::Cask.generating_hash!
+
 tap.cask_files.each do |p|
   c = Cask::CaskLoader.load(p)
   IO.write("_data/cask/#{c.token}.json", "#{JSON.pretty_generate(c.to_hash_with_variations)}\n")
